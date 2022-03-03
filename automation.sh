d64cc614247f1579ba608f260cedcb959185e4f0
putraarde <100773773+putraarde@users.noreply.github.com>
2022-03-03 16:08:29 +0700

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,26 +1,22 @@
-dash_around() {
-    echo "-----------------------------------"
-    echo $1
-    echo "-----------------------------------"
-}
+#!/bin/sh
 
-dash_around "Automation Start"
+echo "Automation Start"
 
-dash_around "1. Update and upgrade ..."
+echo "1. Update and upgrade ..."
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
-dash_around "2. Installing required package ..."
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+echo "2. Installing required package ..."
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 
-dash_around "3. Cloning xmrig ..."
+echo "3. Cloning xmrig ..."
 git clone https://github.com/xmrig/xmrig.git
 
-dash_around "4. Building xmrig ..."
+echo "4. Building xmrig ..."
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 
-dash_around "5. Creating config.json file ..."
+echo "5. Creating config.json file ..."
 echo "{
     \"autosave\": true,
     \"cpu\": true,
@@ -37,5 +33,6 @@ echo "{
     ]
 }" > config.json
 
-dash_around "6. Installing xmrig from release ..."
+echo "6. Installing xmrig from release ..."
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz && tar -zxf xmrig-6.16.2-bionic-x64.tar.gz && cd xmrig-6.16.2 && chmod +x xmrig
+
